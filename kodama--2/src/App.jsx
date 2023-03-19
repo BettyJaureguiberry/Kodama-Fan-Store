3a4883070751317f639af2690ac2b83ef70d8e0a
BettyJaureguiberry <soledadjaureguiberry@gmail.com>
2023-03-19 18:10:41 -0300

    primer prueba

---
+++ kodama--2/src/App.jsx
@@ -0,0 +1,12 @@
+//import logo from './logo.svg'; hay que borrarlo, lo dejo de referencia
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      hola mundo
+    </div>
+  );
+}
+
+export default App;
